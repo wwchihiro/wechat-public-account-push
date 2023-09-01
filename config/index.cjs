a67eedbf7ff28b794116d7d34534498bf84f46c2
wwchihiro <40991694+wwchihiro@users.noreply.github.com>
2023-09-01 10:13:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6FEd6_IFbInW5Rs0vVqWR_vjRj0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	3q7S3ys3fa8nD1KhmXEr6jCzOyfskR6K_D4FKWNxko8',
+      useTemplateId: '3q7S3ys3fa8nD1KhmXEr6jCzOyfskR6K_D4FKWNxko8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
